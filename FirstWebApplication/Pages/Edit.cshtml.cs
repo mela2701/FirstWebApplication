6d3011436f6dbdb601d899525b25ff5357672b92
mela2701 <mohanaelavarasan@gmail.com>
2017-12-05 01:47:58 +0530

    mohanaelavarasan@gmail.com
    swfd

---
+++ FirstWebApplication/Pages/Edit.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FirstWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApplication.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Customer = await _db.Customers.FindAsync(id);
+            if(Customer == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+             _db.Attach(Customer).State = EntityState.Modified;
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException ex)
+            {
+                throw new Exception($"Customer {Customer.Id} not found!");
+            }
+           
+                return RedirectToPage("/Index");
+            
+            
+        }
+    }
+}
---
+++ FirstWebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FirstWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IList<Customer> Customers { get; private set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+           Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var customer = await _db.Customers.FindAsync(id);
+
+            if(customer !=null)
+            {
+                _db.Customers.Remove(customer);
+               await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
